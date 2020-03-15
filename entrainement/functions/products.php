8fda7a2900fd45b96ff7edbb23cd43be8c56ced2
unknown <exodemandah@gmail.com>
2020-03-15 14:29:35 +0100

    repo

--- entrainement/functions/products.php
+++ entrainement/functions/products.php
@@ -5,6 +5,6 @@ function getProducts(): array
 {
   $pdo = getPdo();
 
-  $stmt = $pdo->query("SELECT * FROM produits WHERE visible=1");
+  $stmt = $pdo->query("SELECT * FROM produits WHERE visibble=1");
   return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
